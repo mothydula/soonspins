351024576cac0ca70f1b6575a4fbe5e2868b6c15
timothylukau@email.arizona.edu <Lebron2326>
2020-06-12 18:16:10 -0700

    livestream page styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = ()=> {
     return (
-        <footer class="footer">
-      <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+        <footer className="footer">
+      <div className="container">
+        <span className="text-muted">Place sticky footer content here.</span>
       </div>
     </footer>
     )
--- src/components/Livestream.js
+++ src/components/Livestream.js
@@ -6,7 +6,7 @@ import videojs from 'video.js'
 import VideoPlayer from './VideoPlayer'
 import TwitchPlayerOne from './TwitchPlayer'
 import 'video.js/dist/video-js.css'
-import { Container, Jumbotron } from 'react-bootstrap'
+import { Container, Jumbotron, Row } from 'react-bootstrap'
 import $ from 'jquery'
 import ReactTwitchEmbedVideo from "react-twitch-embed-video"
 
@@ -16,7 +16,7 @@ const Livestream = () => {
     const [artistName, setArtistName] = useState("NULL")
     useEffect(() => {
 
-        let list = $("#twitch-embed");   // Get the <ul> element with id="myList"
+        /*let list = $("#twitch-embed");   // Get the <ul> element with id="myList"
         console.log(list.childNodes)
         let listArr= Array.prototype.slice.call(list.childNodes);
         console.log(listArr)
@@ -25,7 +25,7 @@ const Livestream = () => {
         if(list.childNodes.length > 1){
             console.log("boo")
             list.removeChild(list.childNodes[1]);
-        }      // Remove <ul>'s first child node (index 0)
+        }      // Remove <ul>'s first child node (index 0)*/
     }, [])
     useEffect(() => {
         let list = document.getElementById("twitch-embed");   // Get the <ul> element with id="myList"
@@ -40,6 +40,12 @@ const Livestream = () => {
         <div >
 
             <Header />
+
+            <Jumbotron fluid style={{ backgroundColor: "#F5AD0C", marginBottom: 0 }}>
+                <Container>
+                    <h1 className="title-h1" style={{wordWrap: "break-word"}}>WATCH {artistName}'S LIVESTREAM BELOW</h1>
+                </Container>
+            </Jumbotron>
 
             <Container fluid style={{ height: "100vh", backgroundColor: "#F5AD0C", color: "#BD3712" }}>
 
